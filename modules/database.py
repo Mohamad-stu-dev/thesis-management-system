5cd9ff33efad8d75a9e6d67f14f1650fd5efae16
warfare <mohamadmahmodiiran.2020@gmail.com>
2025-08-18 01:26:02 +0330

    data base for managing json files

---
+++ modules/database.py
@@ -0,0 +1,30 @@
+import json
+import os
+
+current_file_path = os.path.abspath(__file__)  # current file path
+modules_dir = os.path.dirname(
+    current_file_path
+)  # current folder path that database.py is in
+base_dir = os.path.dirname(modules_dir)  # current folder path that modules folder is in
+data_dir = os.path.join(base_dir, "data")  # current folder path that data folder is in
+
+
+def load_data(filename):
+    file_path = os.path.join(data_dir, filename)
+
+    if not os.path.exists(file_path):
+        save_data(filename, [])
+        return []
+
+    with open(file_path, "r", encoding="utf-8") as file:
+        try:
+            data = json.load(file)
+        except json.JSONDecodeError:
+            return []
+
+
+def save_data(filename, data):
+    file_path = os.path.join(data_dir, filename)
+
+    with open(file_path, "w", encoding="utf-8") as file:
+        json.dump(data, file, ensure_ascii=False, indent=4)
